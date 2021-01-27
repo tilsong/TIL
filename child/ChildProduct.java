5ef00d3b620d36d608cba89ee9a55d37f836c0a8
Eunseok Song <cnsong0229@gmail.com>
2021-01-27 11:17:21 +0900

    Delete child directory

--- child/ChildProduct.java
+++
@@ -1,8 +0,0 @@
-package child;
-
-public class ChildProduct<T,M,C> extends Product<T,M>{
-	private C company;
-	
-	public void setCompany(C company) {this.company=company;}
-	public C getCompany() {return this.company;}
-}
--- child/Storage.java
+++
@@ -1,6 +0,0 @@
-package child;
-
-public interface Storage<T> {
-	public void add(T item, int index);
-	public T get(int index);
-}
